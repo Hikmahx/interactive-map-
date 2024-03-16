dc662ba793ebdbe9dcba93aeab463472924fa505
Hikmahx <hikmayousuph@gmail.com>
2024-03-16 22:39:18 +0100

    App.jsx renamed, Map compoent created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./Map";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className>Interactive Map With Leaflet</h1>
+      <div className="">
+        <Map />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Map.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const Map = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{height: '400px'}}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </MapContainer>
+  );
+};
+
+export default Map;
